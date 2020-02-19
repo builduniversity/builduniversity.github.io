277feee17aab51a0c81b952198ecdce4d9441847
kathfinder <syli@cs.washington.edu>
2020-02-20 03:12:23 +0800

    website updates

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Welcome to Build University",
-        "excerpt":"Year 2020 has NOT been off to a good start for most of the world, which is why I am now more motivated than ever to squeeze my finite time on Earth to create useful tech. What I am currently working on, is an app that lets a deaf person...","categories": ["news","blog"],
+        "excerpt":"Year 2020 has NOT been off to a good start for most of the world, which is why I am now more motivated than ever to squeeze my finite time on Earth to create useful tech. What I am currently working on, is an app that lets a deaf person...","categories": ["news"],
         "tags": [],
-        "url": "https://build.university/news/blog/2020/02/16/welcome-to-build-university.html",
+        "url": "https://build.university/news/2020/02/16/welcome-to-build-university.html",
         "teaser":null}]
