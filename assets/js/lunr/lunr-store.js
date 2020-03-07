0f1c8ff6521f15220a11a26f0b3b368dfeb4a423
kathfinder <syli@cs.washington.edu>
2020-03-07 22:27:48 +0800

    website updates

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Welcome to Build University",
         "excerpt":"Year 2020 has NOT been off to a good start for most of the world, which is why I am now more motivated than ever to squeeze my finite time on Earth to create useful tech. What I am currently working on, is an app that lets a deaf person...","categories": ["news"],
         "tags": [],
-        "url": "https://build.university/news/2020/02/16/welcome-to-build-university.html",
+        "url": "https://build.university/news/2020/02/16/welcome-to-build-university/",
         "teaser":null},{
         "title": "How I Built a Startup in One Day",
         "excerpt":"Far from being someone who enjoys competing with others, I somehow like to challenge myself a lot. Merely a day after I got myself out of this “2-Hour Movie Challenge” that I arbitrary created for Leap Day 2020, here I am again, digging another hole for yours truly. But I...","categories": ["news"],
         "tags": [],
-        "url": "https://build.university/news/2020/03/05/building-a-startup-in-one-day.html",
+        "url": "https://build.university/news/2020/03/05/building-a-startup-in-one-day/",
         "teaser":null}]
